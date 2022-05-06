fad500f28578b3a90ed86d7b71e42e0abdba20e9
demiglace0505 <demiglace0505@gmail.com>
2022-05-06 08:30:24 +0800

    pages and routing

---
+++ discodoge-frontend/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>This is an app to find the latest groovy DJ events.</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
